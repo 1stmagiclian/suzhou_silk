695b0b859227d70a2d42047ab2e4eb1265cc925d
zhanglian <2875165755@qq.com>
2023-08-21 21:16:45 +0800

    注释地图放大

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,11 +25,11 @@
         
         <div id="left-top" :class="{ fullscreen: fullScreenStatus.Full }">
           <!-- 商家分布图表 -->
-          <Full ref="full"></Full>
+          <!-- <Full ref="full"></Full>
           <div class="resize">
             <span @click="changeSize('full')" :class="['iconfont', fullScreenStatus.map ? 'icon-compress-alt' : 
             'icon-expand-alt']"></span>
-          </div>
+          </div> -->
         </div>
 
         <div id="left-bottom" :class="{ fullscreen: fullScreenStatus.trend }">
